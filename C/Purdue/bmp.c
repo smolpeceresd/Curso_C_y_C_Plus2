51ce892ebbb472d0cf4335609a21b58fb723645a
Santiago Molpeceres Díaz <santiagomolpeceres@hotmail.com>
2023-07-03 00:12:31 +0200

    Por din

--- C/Purdue/bmp.c
+++ C/Purdue/bmp.c
@@ -9,28 +9,26 @@
 #define BMP_HEADER_SIZE 54
 #define DIB_HEADER_SIZE 40
 
-
 void free_bmp(BMPImage *image)
 {
     free(image);
 }
 
-
 bool check_bmp_header(BMPHeader *bmp_hdr, FILE *fp)
 {
 
     if (
-            bmp_hdr->type == 0x4d42 &&                                                                                 // 1
-            bmp_hdr->offset == BMP_HEADER_SIZE &&                                                                      // 2
-            bmp_hdr->dib_header_size == DIB_HEADER_SIZE &&                                                             // 3
-            bmp_hdr->num_planes == 1 &&                                                                                // 4
-            bmp_hdr->compression == 0 &&                                                                               // 5
-            bmp_hdr->num_colors == 0 &&                                                                                // 6
-            bmp_hdr->important_colors == 0 &&                                                                          // 6
-            (bmp_hdr->bits_per_pixel == 16 || bmp_hdr->bits_per_pixel == 24) &&                                        // 7
-            (((bmp_hdr->bits_per_pixel * bmp_hdr->width_px * bmp_hdr->height_px) / 8) == bmp_hdr->image_size_bytes) && // 8
-            ((((bmp_hdr->bits_per_pixel * bmp_hdr->width_px * bmp_hdr->height_px) / 8) + 54) == bmp_hdr->size)         // 8
-            )
+        bmp_hdr->type == 0x4d42 &&                                                                                 // 1
+        bmp_hdr->offset == BMP_HEADER_SIZE &&                                                                      // 2
+        bmp_hdr->dib_header_size == DIB_HEADER_SIZE &&                                                             // 3
+        bmp_hdr->num_planes == 1 &&                                                                                // 4
+        bmp_hdr->compression == 0 &&                                                                               // 5
+        bmp_hdr->num_colors == 0 &&                                                                                // 6
+        bmp_hdr->important_colors == 0 &&                                                                          // 6
+        (bmp_hdr->bits_per_pixel == 16 || bmp_hdr->bits_per_pixel == 24) &&                                        // 7
+        (((bmp_hdr->bits_per_pixel * bmp_hdr->width_px * bmp_hdr->height_px) / 8) == bmp_hdr->image_size_bytes) && // 8
+        ((((bmp_hdr->bits_per_pixel * bmp_hdr->width_px * bmp_hdr->height_px) / 8) + 54) == bmp_hdr->size)         // 8
+    )
     {
         return true;
     }
@@ -43,21 +41,40 @@ bool check_bmp_header(BMPHeader *bmp_hdr, FILE *fp)
 BMPImage *read_bmp(FILE *fp, char **error)
 {
     BMPHeader *header = (BMPHeader *)malloc(sizeof(BMPHeader)); // declaro la vuelta y reservo su espacio en memoria.
+
+    if (header == NULL)
+    {
+        if (*error == NULL)
+        {
+            char *message = "No hay espacio en memoria para leer la cabecera";
+            *error = malloc((strlen(message) + 1) * sizeof(**error));
+            strcpy(*error, message);
+        }
+        return NULL;
+    }
+
     fread(header, sizeof(BMPHeader), 1, fp);
     if (check_bmp_header(header, fp) == 1)
     {
-        BMPImage *imagen = (BMPImage*)malloc(sizeof(BMPImage));
-        imagen->header=*header;
-        imagen->data =(unsigned char*) malloc(header->image_size_bytes);
-        if(imagen->data==NULL){
-            free_bmp(imagen);
-            char *message = "No hay espacio para leer la imagen";
-            *error = malloc((strlen(message) + 1) * sizeof(**error));
-            strcpy_s(*error,sizeof(error), message);
-            return NULL;
-        }else{
+        BMPImage *imagen = (BMPImage *)malloc(sizeof(BMPImage));
+        imagen->header = *header;
+        imagen->data = (unsigned char *)malloc(header->image_size_bytes);
+        if (imagen->data == NULL)
+        {
+            free(imagen);
+            if (*error == NULL)
+            {
+                char *message = "No hay espacio para leer la imagen";
+                *error = malloc((strlen(message) + 1) * sizeof(**error));
+                strcpy_s(*error, sizeof(error), message);
+            }
 
-            fread(imagen->data,header->image_size_bytes,1,fp);
+            return NULL;
+        }
+        else
+        {
+
+            fread(imagen->data, header->image_size_bytes, 1, fp);
             return imagen;
         }
     }
@@ -67,20 +84,55 @@ BMPImage *read_bmp(FILE *fp, char **error)
         {
             char *message = "La cabecera no se ha extraido bien";
             *error = malloc((strlen(message) + 1) * sizeof(**error));
-            strcpy_s(*error,sizeof(error), message);
+            strcpy_s(*error, sizeof(error), message);
         }
         return NULL;
     }
 }
 
-bool write_bmp(FILE *fp, BMPImage *image, char **error){
+bool write_bmp(FILE *fp, BMPImage *image, char **error)
+{
     // Escribir el encabezado BMP en el archivo
     fwrite(&image->header, sizeof(BMPHeader), 1, fp);
 
     // Copiar los datos de la imagen, cambiando las zonas blancas por rojas
-    unsigned char* dataBuffer = (unsigned char*)malloc(image->header.image_size_bytes);
-    if (dataBuffer == NULL) {
-        *error = "Error al asignar memoria para los datos de la imagen.";
+    unsigned char *dataBuffer = (unsigned char *)malloc(image->header.image_size_bytes);
+    if (dataBuffer == NULL)
+    {
+        if (*error == NULL)
+        {
+            char *message = "Error al asignar memoria para los datos de la imagen.";
+            *error = malloc((strlen(message) + 1) * sizeof(**error));
+            strcpy_s(*error, sizeof(error), message);
+        }
+        return false;
+    }
+
+    memcpy(dataBuffer, image->data, image->header.image_size_bytes); // Copiar los datos de la imagen
+
+    // Escribir los datos de la imagen modificados en el archivo
+    fwrite(dataBuffer, 1, image->header.image_size_bytes, fp);
+
+    // Liberar la memoria asignada
+    free(dataBuffer);
+
+    return true;
+}
+bool changeColor(FILE *fp, BMPImage *image, char **error)
+{
+    // Escribir el encabezado BMP en el archivo
+    fwrite(&image->header, sizeof(BMPHeader), 1, fp);
+
+    // Copiar los datos de la imagen, cambiando las zonas blancas por rojas
+    unsigned char *dataBuffer = (unsigned char *)malloc(image->header.image_size_bytes);
+    if (dataBuffer == NULL)
+    {
+        if (*error == NULL)
+        {
+            char *message = "Error al asignar memoria para los datos de la imagen.";
+            *error = malloc((strlen(message) + 1) * sizeof(**error));
+            strcpy_s(*error, sizeof(error), message);
+        }
         return false;
     }
 
@@ -88,17 +140,16 @@ bool write_bmp(FILE *fp, BMPImage *image, char **error){
 
     int bytesPerPixel = image->header.bits_per_pixel / 8;
     int imageSize = image->header.width_px * image->header.height_px * bytesPerPixel;
-    for (int i = 0; i < imageSize; i += bytesPerPixel) {
+    for (int i = 0; i < imageSize; i += bytesPerPixel)
+    {
+        if (dataBuffer[i] == 204 && dataBuffer[i + 1] == 72 && dataBuffer[i + 2] == 63)
+        {
 
-        if (dataBuffer[i] == 204 && dataBuffer[i + 1] == 72 && dataBuffer[i + 2] == 63) {
-
-            dataBuffer[i] = 35;//azul
-            dataBuffer[i + 1] = 54;// verde
-            dataBuffer[i + 2] = 95;//rojo
+            dataBuffer[i] = 35;     // azul
+            dataBuffer[i + 1] = 54; // verde
+            dataBuffer[i + 2] = 95; // rojo
         }
-
     }
-
 
     // Escribir los datos de la imagen modificados en el archivo
     fwrite(dataBuffer, 1, image->header.image_size_bytes, fp);
@@ -109,5 +160,74 @@ bool write_bmp(FILE *fp, BMPImage *image, char **error){
     return true;
 }
 
+BMPImage *crop_bmp(BMPImage *image, int x, int y, int w, int h, char **error)
+{
+    /*
+    crop bmp(BMPImage✶ image, int x, int y, int w, int h, char✶✶ error)
+→ return type: BMPImage✶
+Create a new image containing the cropped portion of the given image.
+x is the start index, from the left edge of the input image.
+y is the start index, from the top edge of the input image.
+w is the width of the new image.
+h is the height of the new image.
+This will create a new BMPImage, including a BMPHeader that reflects the width and height (and related fields) for the new image (w and h). Copy in pixel data from the original image into the new image data.
+Handle run-time errors using the method described above.
+That means in case of an error, set *error to the address of a descriptive message on the heap and return NULL.
+It is the caller's responsibility to free the memory for the error message.
+Since crop_bmp(…) does not read or write any files, the error handling will be somewhat simpler here.
+    */
 
-BMPImage *crop_bmp(BMPImage *image, int x, int y, int w, int h, char **error);
+    BMPImage *imagen_ = (BMPImage *)malloc(sizeof(BMPImage));
+    if (imagen_ == NULL)
+    {
+        if (*error == NULL)
+        {
+            char *message = "No hay espacio en memoria para leer la cabecera";
+            *error = malloc((strlen(message) + 1) * sizeof(**error));
+            strcpy(*error, message);
+        }
+        return NULL;
+    }
+    imagen_->header = image->header;
+    imagen_->header.width_px = w;
+    imagen_->header.height_px = h;
+    imagen_->header.image_size_bytes = w * h * (image->header.bits_per_pixel / 8);
+    imagen_->header.size = imagen_->header.image_size_bytes + 54;
+    imagen_->data = (unsigned char *)malloc(imagen_->header.image_size_bytes);
+    if (imagen_->data == NULL)
+    {
+        free(imagen_);
+        if (*error == NULL)
+        {
+            char *message = "No hay espacio para leer la imagen";
+            *error = malloc((strlen(message) + 1) * sizeof(**error));
+            strcpy_s(*error, sizeof(error), message);
+        }
+
+        return NULL;
+    }
+    else
+    {
+        int bytesPerPixel = image->header.bits_per_pixel / 8;
+        int imageSize = image->header.width_px * image->header.height_px * bytesPerPixel;
+        int imageSize_ = imagen_->header.width_px * imagen_->header.height_px * bytesPerPixel;
+        int i = 0;
+        int j = 0;
+        for (int k = 0; k < imageSize; k += bytesPerPixel)
+        {
+            if (i == imagen_->header.width_px)
+            {
+                i = 0;
+                j++;
+            }
+            if (i >= x && i < x + w && j >= y && j < y + h)
+            {
+                imagen_->data[i + j * imagen_->header.width_px] = image->data[k];
+                imagen_->data[i + j * imagen_->header.width_px + 1] = image->data[k + 1];
+                imagen_->data[i + j * imagen_->header.width_px + 2] = image->data[k + 2];
+            }
+            i++;
+        }
+        return imagen_;
+    }
+}
--- C/Purdue/main.c
+++ C/Purdue/main.c
@@ -6,49 +6,81 @@
 
 int main()
 {
+
     char *error = NULL;
-    FILE *fichero,*newFichero;
-    fopen_s(&fichero,"CuadradosP.bmp", "rb");
+    FILE *fichero, *imagenCortada,*ImagenColor;
+    fopen_s(&fichero, "CuadradosP.bmp", "rb");
 
-
-    if (fichero == NULL){
+    if (fichero == NULL)
+    {
         printf("\nNo se ha podido abrir el archivo");
-    }else{
+    }
+    else
+    {
         BMPImage *imagen = read_bmp(fichero, &error);
-        if(imagen == NULL){
+        if (imagen == NULL)
+        {
             printf("\nError: %s", error);
             free(error);
-        }else{
+        }
+        else
+        {
             printf("\nImagen leida correctamente");
-            printf("\ntype %x",imagen->header.type            ); // Magic identifier: 0x4d42
-            printf("\nsize %i",imagen->header.size            );// File size in bytes
-            printf("\nreserved1 %i",imagen->header.reserved1       ); // Not used
-            printf("\nreserved2 %i",imagen->header.reserved2       ); // Not used
-            printf("\noffset %i",imagen->header.offset          ); // Offset to image data in bytes from beginning of file (54 bytes)
-            printf("\ndib_header_size %i",imagen->header.dib_header_size ); // DIB Header size in bytes (40 bytes)
-            printf("\nwidth_px %i",imagen->header.width_px         );// Width of the image
-            printf("\nheight_px %i",imagen->header.height_px        );// Height of image
-            printf("\nnum_planes %i",imagen->header.num_planes      );// Number of color planes
-            printf("\nbits_per_pixel %i",imagen->header.bits_per_pixel  ); // Bits per pixel
-            printf("\ncompression %i",imagen->header.compression     ); // Compression type
-            printf("\nimage_size_bytes %i",imagen->header.image_size_bytes); // Image size in bytes
-            printf("\nx_resolution_ppm %i",imagen->header.x_resolution_ppm ); // Pixels per meter
-            printf("\ny_resolution_ppm %i",imagen->header.y_resolution_ppm ); // Pixels per meter
-            printf("\nnum_colors %i",imagen->header.num_colors      ); // Number of colors
-            printf("\nimportant_colors %i",imagen->header.important_colors); // Important colors
+            printf("\ntype %x", imagen->header.type);                         // Magic identifier: 0x4d42
+            printf("\nsize %i", imagen->header.size);                         // File size in bytes
+            printf("\nreserved1 %i", imagen->header.reserved1);               // Not used
+            printf("\nreserved2 %i", imagen->header.reserved2);               // Not used
+            printf("\noffset %i", imagen->header.offset);                     // Offset to image data in bytes from beginning of file (54 bytes)
+            printf("\ndib_header_size %i", imagen->header.dib_header_size);   // DIB Header size in bytes (40 bytes)
+            printf("\nwidth_px %i", imagen->header.width_px);                 // Width of the image
+            printf("\nheight_px %i", imagen->header.height_px);               // Height of image
+            printf("\nnum_planes %i", imagen->header.num_planes);             // Number of color planes
+            printf("\nbits_per_pixel %i", imagen->header.bits_per_pixel);     // Bits per pixel
+            printf("\ncompression %i", imagen->header.compression);           // Compression type
+            printf("\nimage_size_bytes %i", imagen->header.image_size_bytes); // Image size in bytes
+            printf("\nx_resolution_ppm %i", imagen->header.x_resolution_ppm); // Pixels per meter
+            printf("\ny_resolution_ppm %i", imagen->header.y_resolution_ppm); // Pixels per meter
+            printf("\nnum_colors %i", imagen->header.num_colors);             // Number of colors
+            printf("\nimportant_colors %i", imagen->header.important_colors); // Important colors
             puts("\n");
-            fopen_s(&newFichero,"Imagen_salida.bmp","wb");
-            if(newFichero == NULL){
-                printf("\nNo se ha podido abrir el archivo de escritura");
-            }else{
-                bool exito=write_bmp(newFichero,imagen,&error);
-                if(exito==false){
+            fopen_s(&imagenCortada, "Imagen_salida_Cortada.bmp", "wb");
+            fopen_s(&ImagenColor, "Imagen_salida_Color.bmp", "wb");
+            if (imagenCortada == NULL)
+            {
+                printf("\nNo se ha podido abrir el archivo de escritura de corte");
+            }
+            else
+            {
+                /*Quiero usar la funcion crop_bmp partiendo a la mitad la imagen de "imagen"*/
+                BMPImage *imagen2 = crop_bmp(imagen, 0, imagen->header.height_px / 2, imagen->header.width_px, imagen->header.height_px / 2, &error);
+                bool exito = write_bmp(imagenCortada, imagen2, &error);
+                if (exito == false)
+                {
                     printf("\nError: %s", error);
-                }else{
-                    printf("Escrito correctamente");
+                }
+                else
+                {
+                    printf("Corte Escrito correctamente");
                 }
             }
-            fclose(newFichero);
+            if (ImagenColor == NULL)
+            {
+                printf("\nNo se ha podido abrir el archivo de escritura a color");
+            }
+            else
+            {
+                /*Quiero usar la funcion crop_bmp partiendo a la mitad la imagen de "imagen"*/
+                bool exito = changeColor(ImagenColor, imagen, &error);
+                if (exito == false)
+                {
+                    printf("\nError: %s", error);
+                }
+                else
+                {
+                    printf("\nColor Escrito correctamente");
+                }
+            }
+            fclose(imagenCortada);
             fclose(fichero);
             free(imagen);
         }
