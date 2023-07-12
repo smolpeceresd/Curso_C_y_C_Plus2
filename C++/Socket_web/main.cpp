55aaa6cb946781763689a177ff399f9791619f22
Santiago Molpeceres Díaz <santiagomolpeceres@hotmail.com>
2023-07-12 04:22:43 +0200

    Funciona
    Solo quedaría cambiar el hostname para url que tienen HTTP

--- C++/Socket_web/main.cpp
+++ C++/Socket_web/main.cpp
@@ -3,9 +3,14 @@
 #include <string.h>
 #include <ws2tcpip.h>
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <set>
 
-int main()
-{
+using namespace std;
+
+string getHTML(char* hostname_,char *path){
     WSADATA wsaData;
     SOCKET SendingSocket;
     SOCKADDR_IN ServerAddr;
@@ -15,21 +20,19 @@ int main()
     // Initialize Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
     {
-        printf("Failed to initialize Winsock\n");
-        return -1;
+        throw string{"Failed to initialize Winsock\n"};
     }
 
     // Create a socket
     SendingSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (SendingSocket == INVALID_SOCKET)
     {
-        printf("Failed to create socket\n");
         WSACleanup();
-        return -1;
+        throw string{"Failed to create socket\n"};
     }
 
     // Get IP address from domain or URL
-    const char* hostname = "nebrija.es";  // Replace with the domain or URL
+    const char* hostname = hostname_;  // Replace with the domain or URL
     struct addrinfo hints;
     struct addrinfo* result = NULL;
     memset(&hints, 0, sizeof(hints));
@@ -38,10 +41,9 @@ int main()
     hints.ai_protocol = IPPROTO_TCP;
     if (getaddrinfo(hostname, NULL, &hints, &result) != 0)
     {
-        printf("Failed to get IP address\n");
         closesocket(SendingSocket);
         WSACleanup();
-        return -1;
+        throw string{"Failed to get IP address\n"};
     }
     struct sockaddr_in* serverAddr = (struct sockaddr_in*)result->ai_addr;
     char* serverIP = inet_ntoa(serverAddr->sin_addr);
@@ -53,26 +55,26 @@ int main()
     RetCode = connect(SendingSocket, (SOCKADDR*)&ServerAddr, sizeof(ServerAddr));
     if (RetCode != 0)
     {
-        printf("Failed to connect to the server\n");
         closesocket(SendingSocket);
         WSACleanup();
         freeaddrinfo(result);
-        return -1;
+        throw string{"Failed to connect to the server\n"};
     }
 
     // Prepare the HTTP GET request with the hostname
-    std::string request = "GET / HTTP/1.1\r\nHost: ";
+    std::string request = "GET ";
+    request += path;
+    request += " HTTP/1.1\r\nHost: ";
     request += hostname;
     request += "\r\nConnection: close\r\n\r\n";
 
     // Send the HTTP GET request to the server
     if (send(SendingSocket, request.c_str(), request.length(), 0) == SOCKET_ERROR)
     {
-        printf("Failed to send the request\n");
         closesocket(SendingSocket);
         WSACleanup();
         freeaddrinfo(result);
-        return -1;
+        throw string{"Failed to send the request\n"};
     }
 
     // Receive the response from the server and print it
@@ -90,7 +92,117 @@ int main()
     WSACleanup();
     freeaddrinfo(result);
 
-    printf("%s", buffer.c_str());  // Print the content of the buffer as a string
+    // printf("%s", buffer.c_str());  // Print the content of the buffer as a string
 
+    return buffer;
+}
+
+string removeHTMLTags(const std::string& text) {
+    // Expresión regular para buscar etiquetas HTML
+    std::regex tabulador("\t");
+    std::string result = std::regex_replace(text, tabulador, "");
+    std::regex espacio("( )+(?=<)");
+    result=std::regex_replace(result,espacio,"");
+    std::regex etiquetas("<[^>]*>");
+    result=std::regex_replace(result,etiquetas,"");
+
+    return result;
+}
+
+
+string getExtraccionInterior(const string &expresion, const string &texto){
+    std::regex expresion_(expresion);
+    std::string result = std::regex_replace(texto,expresion_, "");
+    return result;
+}
+
+string getExtraccionExacta(const string &expresion, const string &texto){
+    regex hrefPattern("href=\"([^\"]*)\"");
+    smatch match;
+    if (regex_search(texto, match, hrefPattern)) {
+        string hrefValue = match.str(1);
+        return hrefValue;
+    }
+    return "";
+}
+
+set<string> getEnlaces(const string &html){
+
+    /**
+     * @obtengo todos los <a disponibles
+     */
+    regex regexPattern("<a[^>]*>.*?<\\/a>");
+    smatch match;
+    vector<string> aTags;set<string>enlaces;
+
+    string::const_iterator searchStart(html.cbegin());
+    while (regex_search(searchStart, html.cend(), match, regexPattern)) {
+        string aTag = match.str(0);
+        aTags.push_back(aTag);
+        searchStart = match.suffix().first;
+    }
+
+    /**
+     * @De estos debo separar el enlace y el texto
+     */
+    // Imprimir las etiquetas <a> encontradas
+    string href,texto;
+    cout << "Etiquetas <a> encontradas:" << endl;
+    for (const string& aTag : aTags) {
+        href=getExtraccionExacta("href=\"([^\"]*)\"",aTag);
+        if(href.at(0)=='/' || href.at(0)=='h'){
+
+            enlaces.insert(href);
+        }
+        //  texto=getExtraccionInterior("(<([^>]+)>)(.*?)(<//([a-z]+)>)*",aTag);
+        // cout<<"\n"<<aTag<<"\nHREF: "<<href<<"\nTexto: "<<texto;
+    }
+    return enlaces;
+}
+
+int main()
+{
+    int eleccion{0};
+    string buffer,hostname="168.83.78.194",path="/";;
+    do{
+        cout<<"\nhostname: "<<hostname<<" path: "<<path;
+        try {
+            buffer = getHTML(const_cast<char*>(hostname.c_str()),const_cast<char*>(path.c_str()));
+
+        } catch(string error) {
+            cout<<"\nERROR: "<<error;
+        }
+        int body = buffer.find("<body");
+        buffer=buffer.substr(body,(buffer.size()-1)-body);
+        set<string> enlaces_= getEnlaces(buffer);
+        vector<string> enlaces{enlaces_.begin(),enlaces_.end()};
+        buffer = removeHTMLTags(buffer);
+        ofstream fichero;fichero.open("HTML.txt",ios::out | ios::trunc);
+        fichero<<buffer;
+        fichero.close();
+        cout<<"\n Se ha creado un fichero HTML para la lectura del HTML.";
+
+        cout<<"\n A donde quieres navegar?";
+        for(int i=0; i< enlaces.size();i++){
+            /*   if(enlaces.at(i).at(0)=='/'){
+                cout<<"\n"<<i<<". http://"<<hostname<<enlaces.at(i);
+            }else{
+
+                cout<<"\n"<<i<<". "<<enlaces.at(i);
+            }*/
+            cout<<"\n"<<i<<". "<<enlaces.at(i);
+        }
+        cout<<"\n -1 Para salir";
+        cout<<"\n IR ";cin>>eleccion;
+        if(eleccion!=-1){
+
+            if(enlaces.at(eleccion).at(0)=='/'){
+                path=enlaces.at(eleccion);
+            }
+        }
+
+        system("cls");
+    }while (eleccion!=-1);
+    cout<<"Finalizado \n\n";
     return 0;
 }
